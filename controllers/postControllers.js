e62863759e8626d47db2ec333e79e56ba8e54924
Ansal Jain <jainansal@gmail.com>
2023-06-07 12:43:39 +0530

    post-controllers-added

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,19 +1,20 @@
 import Post from "../models/PostModel.js";
 
+// Create new post
 export const createPost = async (req, res) => {
     try {
         // console.log(req.user);
         const title = req.body.title;
-        if(!title) {
+        if (!title) {
             res.status(400).json('No title provided.');
             return;
         }
         let summary = '';
         let content = '';
         let image = '';
-        if(req.body.summary) summary = req.body.summary;
-        if(req.body.content) content = req.body.content;
-        if(req.body.picturePath) image = req.body.picturePath;
+        if (req.body.summary) summary = req.body.summary;
+        if (req.body.content) content = req.body.content;
+        if (req.body.picturePath) image = req.body.picturePath;
         const PostDoc = await Post.create({
             title,
             summary,
@@ -22,12 +23,70 @@ export const createPost = async (req, res) => {
             author: req.user.id
         });
 
-        if(!PostDoc) {
+        if (!PostDoc) {
             res.status(400).json(`Some error occured while creating the post.`)
         } else {
             res.status(200).json(PostDoc);
         }
-    } catch(err) {
-        res.status(500).json({msg: err});
+    } catch (err) {
+        res.status(500).json({ msg: err });
     }
 };
+
+// Get recent posts
+export const getRecent = async (req, res) => {
+    try {
+        const recent = await Post.find()
+            .populate('author', ['username'])
+            .sort({ createdAt: -1 })
+            .limit(10);
+
+        res.status(200).json(recent);
+    } catch (err) {
+        res.status(500).json({ msg: err });
+    }
+};
+
+// Get post by id
+export const getOne = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const postInfo = await Post.findById(id).populate('author', ['username']);
+        res.status(200).json(postInfo);
+    } catch (err) {
+        res.status(500).json({ msg: err });
+    }
+};
+
+
+// Update post by id
+export const updateOne = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const reqPost = await Post.findById(id);
+
+        if(!reqPost) {
+            res.status(400).json('No such post exists.');
+            return;
+        } 
+
+        const isAuthor = JSON.stringify(req.user.id) === JSON.stringify(reqPost.author);
+
+        if(!isAuthor) {
+            res.status(400).json('Unauthorized access.');
+            return;
+        }
+
+        if(req.body.title) reqPost.title = req.body.title;
+        if(req.body.summary) reqPost.summary = req.body.summary;
+        if(req.body.content) reqPost.content = req.body.content;
+        if(req.body.image) reqPost.image = req.body.image;
+
+        reqPost.save();
+
+        res.status(200).json(reqPost);
+
+    } catch (err) {
+        res.status(500).json({ msg: err });
+    }
+}
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,22 @@
 import express from "express";
 import { verifyToken } from "../middleware/authMiddleware.js";
-import { createPost } from "../controllers/postControllers.js";
+import {
+    createPost,
+    getRecent,
+    getOne,
+    updateOne
+} from "../controllers/postControllers.js";
 
 const postRoutes = express.Router();
 
-// New post
+// Create
 postRoutes.post('/add', verifyToken, createPost);
+
+// Read
+postRoutes.get('/recent', getRecent);
+postRoutes.get('/:id', getOne);
+
+// Update
+postRoutes.put('/:id', verifyToken, updateOne);
 
 export default postRoutes;
